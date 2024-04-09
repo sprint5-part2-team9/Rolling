d55cea3fb9a56c5b15781300ad6df99ed177d6ca
Park Sumin <fhsjdvs@gmail.com>
2024-04-09 15:27:37 +0900

    Feat: 버튼 없는 헤더 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./global.scss";
 import "./reset.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Post from "./pages/Post/Post";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/post" element={<Post />}/>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,7 +1,13 @@
 // Post.jsx
+import Header from "../../components/Header";
+
 
 function Post() {
-  return <></>;
+  return (
+    <>
+      <Header isbutton = {false}/>
+    </>
+  );
 }
 
 export default Post;
