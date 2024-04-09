ac58ea3afcf0e84fcb8f811399ea5b14a11b28cd
이현승 <superkail13@gmail.com>
2024-04-09 11:36:57 +0900

    feat: 헤더

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
 // Header.jsx
+import styles from './Header.module.scss';
+import { Link } from 'react-router-dom';
+
 function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className={styles.header}>
+      <div className={styles.frame}>
+        <Link to="/">
+          <h1 className={`${styles.title}`}>Rolling</h1>
+        </Link>
+        <Link to="/post">
+          <button type="button">롤링 페이퍼 만들기</button>
+        </Link>
+      </div>
     </header>
   );
 }
+//위 버튼은 컴포넌트 스타일을 적용할 것임
 
 export default Header;
