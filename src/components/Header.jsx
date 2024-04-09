8899e03ac541766634f515fcf64f70bb8bce1bbe
박수민 <141231525+ssumai-kr@users.noreply.github.com>
2024-04-09 12:49:11 +0900

    Revert "feat: 헤더"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,10 @@
 // Header.jsx
-import styles from './Header.module.scss';
-import { Link } from 'react-router-dom';
-
 function Header() {
   return (
-    <header className={styles.header}>
-      <nav className={styles.frame}>
-        <Link to="/">
-          <h1 className={`${styles.title}`}>Rolling</h1>
-        </Link>
-        <Link to="/post">
-          <button type="button">롤링 페이퍼 만들기</button>
-        </Link>
-      </nav>
+    <header>
+      <h1>Header</h1>
     </header>
   );
 }
-//위 버튼은 컴포넌트 스타일을 적용할 것임
 
 export default Header;
