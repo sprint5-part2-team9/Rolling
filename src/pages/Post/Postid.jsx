ae086878179c4fc7b73c0805f898a640f351b88f
waterkail <41028065+waterkail@users.noreply.github.com>
2024-04-11 17:54:21 +0900

    rename: 깃허브의 api 이름이 변경되지 않은 것을 보여 다시 API로 이름 변경

---
+++ src/pages/Post/Postid.jsx
@@ -0,0 +1,13 @@
+import Header from '../../components/Header';
+import Subheader from '../../components/Subheader';
+
+function PostId() {
+  return (
+    <>
+      <Header />
+      <Subheader />
+    </>
+  );
+}
+
+export default PostId;
