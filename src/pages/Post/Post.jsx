e3fac302ed8c300974971ce65e01d89e9f6c2f2a
jimin <qq6284@naver.com>
2024-04-11 14:53:58 +0900

    Feat: Post 페이지에 서브헤더 반영

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,14 +1,16 @@
 // Post.jsx
-import Header from "../../components/Header";
-import PostMain from "../../components/PostMain";
+import Header from '../../components/Header';
+import PostMain from '../../components/PostMain';
+import Subheader from '../../components/Subheader';
 
 //Post Page
 
 function Post() {
   return (
     <>
-      <Header isbutton = {false}/>
-      <PostMain/>
+      <Header isbutton={false} />
+      <Subheader />
+      <PostMain />
     </>
   );
 }
