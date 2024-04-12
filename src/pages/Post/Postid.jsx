5aaff43c80348f53a484e2eab759bc3fc6446e61
waterkail <41028065+waterkail@users.noreply.github.com>
2024-04-12 19:07:48 +0900

    feat: 페이지네이션 기능 완성

--- src/pages/Post/Postid.jsx
+++ src/pages/Post/Postid.jsx
@@ -5,18 +5,39 @@ import Subheader from "../../components/Subheader";
 import Main from "../../components/Main";
 import { getMessages, deleteMessage } from "../../Api/Api";
 import Cards from "../../components/PostId/Cards";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState, useRef, useMemo } from "react";
 import { useParams } from "react-router-dom";
+
+const FIRST_LIMIT = 11;
+const LIMIT = 6;
 
 function PostId({ edit }) {
   const { postId } = useParams();
   const [messages, setMessages] = useState([]);
   const [isLoading, setisLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const pageEnd = useRef(null);
+  let offset = useRef(FIRST_LIMIT);
+  let counts = useRef(0);
+
+  const onIntersect = async ([entry], observer) => {
+    if (!isLoading && entry.isIntersecting && offset.current < counts.current) {
+      try {
+        observer.unobserve(entry.target);
+        getData(getMessages, postId, LIMIT, offset.current);
+        setTimeout(() => {
+          observer.observe(entry.target);
+        }, 1000);
+      } catch (err) {
+      } finally {
+        setisLoading(false);
+        offset.current = offset.current + 6;
+      }
+    }
+  };
 
   const handleDelete = async (e) => {
     const target = Number(e.target.name);
-
     try {
       await deleteMessage(target);
       setMessages((prev) => prev.filter((item) => item?.id !== target));
@@ -29,8 +50,13 @@ function PostId({ edit }) {
     setisLoading(true);
     setIsError(false);
     try {
-      const { results } = await asyncFunction(id, limit, offset);
-      setMessages(results);
+      const { results, count } = await asyncFunction(id, limit, offset);
+      if (offset === 0) {
+        setMessages(results);
+      } else {
+        setMessages((prev) => [...prev, ...results]);
+      }
+      counts.current = count;
     } catch (err) {
       console.log(err);
       setIsError(true);
@@ -39,21 +65,31 @@ function PostId({ edit }) {
     }
   }, []);
 
+  const observer = useCallback(
+    new IntersectionObserver(onIntersect, { threshold: 0.5 }),
+    []
+  );
+
   useEffect(() => {
-    getData(getMessages, postId, 8, 0);
-  }, [getData, postId]);
+    getData(getMessages, postId, FIRST_LIMIT, 0);
+    if (pageEnd.current) observer.observe(pageEnd.current);
+  }, [getData, postId, observer]);
 
   return (
     <>
       <Header />
       {/* <Subheader /> */}
       <Main>
-        <Cards
-          items={messages}
-          onClick={handleDelete}
-          edit={edit}
-          postId={postId}
-        />
+        <div>
+          <Cards
+            items={messages}
+            onClick={handleDelete}
+            edit={edit}
+            postId={postId}
+          />
+          {isLoading && <div>로딩중...</div>}
+          <div style={{ height: "10px" }} ref={pageEnd}></div>
+        </div>
       </Main>
     </>
   );
