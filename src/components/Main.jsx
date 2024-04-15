6b1911a44c7a131611c621a48f480603e13ae4f3
MEGUMMY1 <cys10902@nate.com>
2024-04-15 20:16:18 +0900

    Design: Landing Page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -36,15 +36,11 @@ function Main({ children, bgColor = "", bgImg = null }) {
           <div className={styles.description}>롤링 페이퍼에 이모지를 추가할 수 있어요.</div>
         </div>
       </section>
-      <footer>
-        <button className={styles.linkButton}>
-          <Link to='/list' className={styles.linkToList}>
-            구경해보기
-          </Link>
-        </button>
-      </footer>
+      <Link to='/list'>
+        <div className={styles.linkButton}>구경해보기</div>
+      </Link>
     </div>
   );
-};
+}
 
 export default Main;
