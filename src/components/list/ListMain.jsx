0722734113db4e0e13d6fb1fb27cf3e7fe5ad4d5
GoldenHamsterK <kimdajin0928@gmail.com>
2024-04-15 12:55:30 +0900

    Feat : cardList API getRecipientsList 연결

--- src/components/list/ListMain.jsx
+++ src/components/list/ListMain.jsx
@@ -1,14 +1,29 @@
 import styles from "./ListMain.module.scss";
 import ToCardListBest from "./ToCardListBest";
 import ToCardListNew from "./ToCardListNew";
+import { getRecipientsList } from "../../Api/Api";
 
-import { useState, createContext } from "react";
-import Data from "./Data.json";
+import { useState, createContext, useEffect } from "react";
+
+// import Data from "./Data.json";
+
 
 export const RollingPaperContext = createContext();
 
 const ListMain = () => {
-  const [rollingPaperData, setRollingPaperData] = useState(Data);
+  const [rollingPaperData, setRollingPaperData] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getRecipientsList();
+        setRollingPaperData(data.results);
+      } catch (error) {
+        console.error("에러입니다.");
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <RollingPaperContext.Provider value={rollingPaperData}>
