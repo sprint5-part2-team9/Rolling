b67623514211274306908781eb01e9477f3058d6
MEGUMMY1 <cys10902@nate.com>
2024-04-17 13:28:09 +0900

    Design: 반응형 수정

--- src/components/PostMain.jsx
+++ src/components/PostMain.jsx
@@ -64,33 +64,39 @@ function PostMain() {
   return (
     <main className={styles.PostMain}>
       <form>
-        <label className={`${styles.to} ${styles.label}`}>To.</label>
-        <Input
-          id='name'
-          placeholder='받는 사람 이름을 입력해 주세요.'
-          value={name}
-          onChange={handleNameChange}
-        />
-        <label className={styles.choose}>배경화면을 선택해 주세요.</label>
-        <p className={styles.text}>컬러를 선택하거나, 이미지를 선택할 수 있습니다.</p>
-        <div className={styles.toggle}>
-          <button
-            type='button'
-            name='color'
-            className={`${styles.toggleButton} ${selectedButton === "color" ? styles.checked : styles.unchecked}`}
-            onClick={() => handleToggleClick("color")}
-          >
-            컬러
-          </button>
-          <button
-            type='button'
-            name='image'
-            className={`${styles.toggleButton} ${selectedButton === "image" ? styles.checked : styles.unchecked}`}
-            onClick={() => handleToggleClick("image")}
-          >
-            이미지
-          </button>
-        </div>
+        <section className={styles.section}>
+          <label className={styles.to}>To.</label>
+          <Input
+            id='name'
+            placeholder='받는 사람 이름을 입력해 주세요.'
+            value={name}
+            onChange={handleNameChange}
+          />
+        </section>
+        <section className={styles.section}>
+          <label className={styles.choose}>배경화면을 선택해 주세요.</label>
+          <p className={styles.text}>컬러를 선택하거나, 이미지를 선택할 수 있습니다.</p>
+        </section>
+        <section className={styles.section}>
+          <div className={styles.toggle}>
+            <button
+              type='button'
+              name='color'
+              className={`${styles.toggleButton} ${selectedButton === "color" ? styles.checked : styles.unchecked}`}
+              onClick={() => handleToggleClick("color")}
+            >
+              컬러
+            </button>
+            <button
+              type='button'
+              name='image'
+              className={`${styles.toggleButton} ${selectedButton === "image" ? styles.checked : styles.unchecked}`}
+              onClick={() => handleToggleClick("image")}
+            >
+              이미지
+            </button>
+          </div>
+        </section>
         <div className={styles.btn__container}>
           <Option ColorOrImg={selectedButton} setBackGround={pickBackgorund} />
           <div className={styles.btns}>
