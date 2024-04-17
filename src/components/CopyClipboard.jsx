c80a46f022718e2f5bb7f4fed446e935465c14d0
jimin <qq6284@naver.com>
2024-04-17 18:38:07 +0900

    Design: 드롭다운

--- src/components/CopyClipboard.jsx
+++ src/components/CopyClipboard.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import styles from './CopyClipboard.module.scss';
 
 function CopyClipboard() {
   const copyClipboard = () => {
@@ -9,18 +10,22 @@ function CopyClipboard() {
     navigator.clipboard
       .writeText(url)
       .then(() => {
-        toast.success("URL이 클립보드에 복사되었습니다.", {
-          position: "bottom-center",
+        toast.success('URL이 클립보드에 복사되었습니다.', {
+          position: 'bottom-center',
         });
       })
       .catch((err) => {
-        toast.error("URL 복사 실패: " + err.message, {
-          position: "bottom-center",
+        toast.error('URL 복사 실패: ' + err.message, {
+          position: 'bottom-center',
         });
       });
   };
 
-  return <li onClick={copyClipboard}>URL 공유</li>;
+  return (
+    <li className={styles.copyButton} onClick={copyClipboard}>
+      URL 공유
+    </li>
+  );
 }
 
 export default CopyClipboard;
